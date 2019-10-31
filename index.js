cf4b9626ca380777827d6fea28cd846efc8d1684
Matti Nilsson <matti.nilsson980105@gmail.com>
2019-10-31 16:27:48 +0100

    Finished. (Fixed things I overlooked)

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ fruitTitle.childNodes[1].setAttribute("style", "color: red;");
 let a = document.getElementsByTagName("a");
 console.log(a);
 a[2].innerText = "Vegetables";
+a[2].href = "#vegetables";
 
 // Added a header to contact
 let contact = document.createElement("h2");
@@ -18,20 +19,35 @@ let contactObj = document.getElementById("contact")
 contactObj.insertBefore(contact, contactObj.childNodes[0]);
 contact.textContent = "Contact";
 
-// Switched about place to become first child of Main
+// Switched about place to become first child of Main & fix the P-tag
 let aboutObj = document.getElementById("about");
 let main = document.getElementById("main");
+let aboutP = document.createElement("p");
 main.insertBefore(aboutObj, main.childNodes[0]);
 console.log(main);
 let about = document.createElement("h2");
 about.textContent = "About";
 aboutObj.appendChild(about);
-aboutObj.insertBefore(about, aboutObj.childNodes[0])
-
-// Make Name and Email Appear A little more Centered
+aboutObj.insertBefore(about, aboutObj.childNodes[0]);
+aboutObj.childNodes[1].remove();
+aboutObj.insertBefore(aboutP, aboutObj.childNodes[1]);
+aboutP.textContent = "We're the best in fruits & vegetables"
+// switch "td" to "th"
 let tHead = document.getElementsByTagName("thead")[0];
-console.log(tHead);
 let tR = tHead.childNodes[1];
-console.log(tR);
-tR.childNodes[1].setAttribute("style", "position: relative; left : 18px; font-weight:bold;");
-tR.childNodes[3].setAttribute("style","display : flex; justify-content: center; font-weight:bold;");
+tR.childNodes[1].remove(); 
+tR.childNodes[2].remove(); 
+let thOne = document.createElement("th");
+tR.appendChild(thOne);
+thOne.textContent = "Name";
+let thTwo = document.createElement("th");
+tR.appendChild(thTwo);
+thTwo.textContent = "Email";
+
+// Link Css file to Html
+head = document.getElementById("head");
+linkCss = document.createElement("link");
+head.appendChild(linkCss);
+linkCss.rel = "stylesheet";
+linkCss.type = "text/css";
+linkCss.href = "main.css";
